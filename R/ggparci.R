7f3a891164fc4ada5fae3b239754160fa0879662
Shay Yaacoby <shay66@gmail.com>
2017-12-27 16:22:52 +0200

    Version adapted for the MIP: * theme change * different function arguments

--- R/ggparci.R
+++ R/ggparci.R
@@ -1,134 +1,138 @@
 # ggparci
 
-
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param x should be a data.frame with each column representing a data dimension
-#' @param group a grouping variable
-#' @param level PARAM_DESCRIPTION, Default: 0.95
-#' @param lab_group PARAM_DESCRIPTION, Default: ''
-#' @param lab_y PARAM_DESCRIPTION, Default: ''
-#' @param lab_x PARAM_DESCRIPTION, Default: ''
-#' @param title PARAM_DESCRIPTION, Default: ''
-#' @param ylim PARAM_DESCRIPTION
-#' @param include_lines PARAM_DESCRIPTION, Default: FALSE
-#' @param seriate PARAM_DESCRIPTION, Default: TRUE
-#' @param facet PARAM_DESCRIPTION, Default: FALSE
-#' @param flip PARAM_DESCRIPTION, Default: FALSE
-#' @param alpha_per_line PARAM_DESCRIPTION, Default: 0.1
-#' @param alpha_geom_ribbon PARAM_DESCRIPTION, Default: 0.5
-#' @param jitter_median_factor PARAM_DESCRIPTION, Default: 0
-#' @param ... PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Parallel Coordinates Plot with Groups medians and their Confidence Intervals
+#' @description The function plots parallel coordinates (gg)plot, in which each line connects the variables medians for each group.
+#'              Confidence bands for the medians are added to each line. This allows the assessment of groups (clusters) separations.
+#'              The variables are normalized to [0,1] sclae prior to plotting.
+#' @param data a data.frame. includes the grouping variable
+#' @param columns variables to include in the plot, as character vector with columns names or as integer vector with positions. the group variable is ignored, if specified.
+#' @param groups_column grouping variable name (quoted)
+#' @param conf_level confidence level for the bands, Default: 0.95
+#' @param title Plot title, Default: ''
+#' @param groups_in_facets logical. plot each group in a separate plot/facet? Default: FALSE
+#' @param flip_coords logical. make the coordinates horizontal? Default: FALSE
+#' @param obs_lines logical. add lines for the rows? Default: FALSE
+#' @param alpha_lines transparency factor for the lines, Default: 0.1
+#' @param alpha_bands transparency factor for the confidence bands, Default: 0.5
+#' @param ... additional arguments to the plot
+#' @return a ggplot object
 #'
 #' @examples
 #'
 #' \dontrun{
 #'
-#' ggparci(iris[,-5], iris[,5])
-#' ggparci(normalize(iris[,-5]), iris[,5], ylim = c(0,1))
-#' ggparci(percentize(iris[,-5]), iris[,5], ylim = c(0,1))
-#' ggparci(x = percentize(mtcars[,-10]),group =  factor(mtcars[,10]), ylim = c(0,1))
-#' ggparci(x = percentize(mtcars[,-9]),group =  factor(mtcars[,9]), ylim = c(0,1), lab_group = "automatic\nvs\nmanual")
+#' ggparci(iris, groups_column = "Species")
+#' ggparci(normalize(iris), groups_column = "Species") # already normalized in this version
 #'
-#' ggparci(x = percentize(mtcars[,-9]),group =  factor(mtcars[,9]), jitter_median_factor = 0)
-#' ggparci(x = percentize(mtcars[,-9]),group =  factor(mtcars[,9]), jitter_median_factor = 5)
+#' #select only some of the variables
+#' # plot a line for each observation
+#' # and dont plot the CIs
+#' ggparci(mtcars,columns = 3:7, groups_column = "carb", obs_lines = T, alpha_bands = 0)
 #'
-#' p <- ggparci(x = percentize(mtcars[,-9]),group =  factor(mtcars[,9]), ylim = c(0,1), lab_group = "automatic\nvs\nmanual", flip = TRUE)
-#' library(plotly)
-#' ggplotly(p)
+#' # display each group in a different facet
+#' ggparci(data = iris, groups_column = "Species", groups_in_facets = T)
 #'
+#' # flip the plot
+#' p <- ggparci(data = iris, groups_column = "Species", flip_coords = T)
 #'
 #' }
-#'
 #' @rdname ggparci
 #' @export
-ggparci <- function(x, group, level = 0.95,
-                          lab_group = "",
-                          lab_y = "", lab_x = "",
-                          title = "",
-                          ylim,
-                          include_lines = FALSE,
-                          seriate = FALSE,
-                          facet = FALSE,
-                          flip = FALSE,
-                          alpha_per_line = 0.1,
-                          alpha_geom_ribbon = 0.5,
-                          jitter_median_factor = 0,
-                          ...
-) {
 
-  x <- as.data.frame(x)
-  if(missing(group)) group <- rep(1, nrow(x))
-  x$group <- factor(group)
-  alpha <- 1-level
+ggparci <- function(data, columns = 1:ncol(data), groups_column, conf_level = 0.95, title = "",
+  groups_in_facets = FALSE, flip_coords = FALSE, obs_lines = FALSE, alpha_lines = 0.2,alpha_bands = 0.5,...)
+  {
+  numeric_columns <- columns[sapply(data[columns], is.numeric)]
+  if (is.numeric(data[,groups_column]))
+      data[,groups_column] <- as.character(data[,groups_column])
 
-  tmp <- x %>% # iris %>% # percentize %>%
-    dplyr::mutate(row_id = seq_len(n())) %>%  tidyr::gather(key = "measures", value = "value", -row_id, -group)
-  # head(tmp)
-  # https://stats.stackexchange.com/a/21116/253
+  alpha <- 1-conf_level
 
-  # some ideas from here:
-  # http://stackoverflow.com/questions/14033551/r-plotting-confidence-bands-with-ggplot
-  predframe <-
-    tmp %>% dplyr::group_by(measures, group) %>%
-    dplyr::summarise(median = median(value), n = n(),
-              L = sort(value)[max(qbinom(alpha/2, n(), 0.5), 1)], # the max is to deal with cases of 0
-              U = sort(value)[qbinom((1-alpha/2), n(), 0.5)])
-  # sort(value)[qbinom(c(.025,.975), n(), 0.5)]
+  data_to_plot <-
+    data %>%
+    as_data_frame %>%
+    dplyr::select(groups_column, numeric_columns) %>%
+    dplyr::mutate_if(is.numeric,ggparci:::normalize.default) %>%
+    tibble::rownames_to_column("obs_id") %>%
+    tidyr::gather(
+      key = "Variables", value = "value", -!!groups_column, -obs_id)
 
-  if(seriate) {
-    measures_df <- predframe %>% dplyr::select(1:3) %>% tidyr::spread(key = group, value = median) %>%
-      data.frame
-    rownames(measures_df) <- measures_df[,1]
-    measures_df <- measures_df[,-1]
-    # TODO: control the dist measure
-    the_dist <- seriation::dist(measures_df)
-    # library(seriation)
-    # library(dendextend)
-    # seriate_dendrogram
-    # TODO: control the seriate method.
-    # TSP has a random element, we wish to set it to always give the same result
-    # set.seed(2017-05-06)
-    ss_c <- seriation::get_order(seriate(the_dist, method = "TSP"))
-    seriated_measures <- rownames(measures_df)[ss_c]
-    tmp$measures <- factor(tmp$measures, levels = seriated_measures)
-    predframe$measures <- factor(predframe$measures, levels = seriated_measures)
+  if (obs_lines)
+    obs_data_to_plot <-
+      data_to_plot %>%
+      dplyr::group_by(obs_id)
+
+  summ_data_to_plot <-
+    data_to_plot %>%
+    dplyr::group_by(Variables) %>%
+    dplyr::group_by_(groups_column, add = TRUE) %>%
+    dplyr::summarise(
+      median = median(value),
+      L = sort(value)[max(qbinom(alpha/2, n(), 0.5), 1)],
+      U = sort(value)[qbinom((1-alpha/2), n(), 0.5)])
+
+  p <- ggplot() +
+    aes( x = Variables ) +
+    aes_string( color = groups_column )
+
+  if (obs_lines)
+  {
+    p <- p +
+      geom_line(
+        data = obs_data_to_plot,
+        aes(group = obs_id, y = value),
+        show.legend = FALSE, alpha = alpha_lines)
   }
 
-  # jittering should be done AFTER seriation.
-  if(jitter_median_factor > 0) predframe$median <- jitter(predframe$median, factor = jitter_median_factor)
+  p <- p +
+    aes_string(
+    color = groups_column,
+    fill  = groups_column,
+    group = groups_column) +
+    geom_line(
+      data = summ_data_to_plot,
+      aes(y = median)) +
+    geom_ribbon(
+      data = summ_data_to_plot,
+      aes(ymin = L, ymax = U),
+      alpha = alpha_bands, color =  adjustcolor( "grey", alpha.f = alpha_bands)) +
+    labs(title = title) +
+    scale_y_continuous(breaks = c((0:10)/10)) +
+    scale_x_discrete(expand = c(0.05,0))
 
+  p <- p + theme_bw()
 
-  p <- ggplot(data = tmp, aes(x = measures, color = group))
+  if(groups_in_facets)
+    p <- p + facet_grid(reformulate(groups_column,".")) + theme(legend.position="none") + scale_x_discrete(expand = waiver())
 
-  if(include_lines) {
-    p <- p + geom_line(aes(y = value, group = row_id), size = 0.8, alpha = alpha_per_line)
+  if(flip_coords)
+  {
+    p <- p + coord_flip() +
+      theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_blank(),
+        axis.line.x  = element_line(colour = "grey"),
+        # axis.ticks.y = element_blank(),
+        # axis.text.y = element_blank(),
+        #panel.grid.minor.y=element_blank(),
+        panel.grid.minor.x=element_blank())
+    if(groups_in_facets)
+      p <- p + facet_grid(reformulate(".",groups_column))
+  } else
+  {
+    p <- p +
+      theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.border = element_blank(),
+        axis.line.y  = element_line(colour = "grey"),
+        # axis.ticks.y = element_blank(),
+        # axis.text.y = element_blank(),
+        #panel.grid.minor.y=element_blank(),
+        panel.grid.minor.y=element_blank(),
+        axis.text.x=element_text(angle=90, hjust=1)
+      )
   }
-
-  p <- p + geom_ribbon(data = predframe, aes(ymin = L, ymax = U, fill = group,
-                                      group = group),
-                alpha = alpha_geom_ribbon, # fill = "grey",
-                color =  adjustcolor( "grey", alpha.f = 0.1)) +
-    geom_line(data = predframe, aes(y = median, color = group,
-                                    group = group), size = 2, alpha = 0.8) +
-    # lims(y = c(0,1)) +
-    # theme(axis.text.x=element_text(angle=90, hjust=1) ) +
-    labs(color = lab_group, y = lab_y, x = lab_x, title = title)
-
-  if(!missing(ylim)) p <- p + lims(y = ylim)
-
-  # http://stackoverflow.com/questions/14604435/turning-off-some-legends-in-a-ggplot
-  p <- p + guides(fill=FALSE) +
-    guides(color = guide_legend(override.aes = list(alpha = 1)))
-  # fix legend alpha becoming transperant
-  # http://stackoverflow.com/questions/5290003/how-to-set-legend-alpha-with-ggplot2
-
-
-  if(facet) p <- p + facet_grid(group ~ .)
-  if(flip) p <- p + coord_flip()
-  p
+  return(p)
 }
-
-
