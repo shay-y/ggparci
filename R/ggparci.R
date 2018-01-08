266dc3d9a8f9387e666213e9d02e97c0c60e36d6
Shay Yaacoby <shay66@gmail.com>
2018-01-08 17:48:20 +0200

    remove normalize out of the function adjust examples accordingly change theme back to grey make parameter names more clear

--- R/ggparci.R
+++ R/ggparci.R
@@ -11,9 +11,9 @@
 #' @param title Plot title, Default: ''
 #' @param groups_in_facets logical. plot each group in a separate plot/facet? Default: FALSE
 #' @param flip_coords logical. make the coordinates horizontal? Default: FALSE
-#' @param obs_lines logical. add lines for the rows? Default: FALSE
-#' @param alpha_lines transparency factor for the lines, Default: 0.1
-#' @param alpha_bands transparency factor for the confidence bands, Default: 0.5
+#' @param include_obs_lines logical. add lines for the rows? Default: FALSE
+#' @param alpha_obs_lines transparency factor for the lines, Default: 0.1
+#' @param alpha_ci_bands transparency factor for the confidence bands, Default: 0.5
 #' @param ... additional arguments to the plot
 #' @return a ggplot object
 #'
@@ -22,46 +22,60 @@
 #' \dontrun{
 #'
 #' ggparci(iris, groups_column = "Species")
-#' ggparci(normalize(iris), groups_column = "Species") # already normalized in this version
+#' ggparci(normalize(iris), groups_column = "Species")
+#' ggparci(percentize(iris), groups_column = "Species")
 #'
 #' #select only some of the variables
 #' # plot a line for each observation
 #' # and dont plot the CIs
-#' ggparci(mtcars,columns = 3:7, groups_column = "carb", obs_lines = T, alpha_bands = 0)
+#' ggparci(percentize(iris),columns = 1:3, groups_column = "Species", include_obs_lines = T, alpha_ci_bands = 0)
 #'
 #' # display each group in a different facet
-#' ggparci(data = iris, groups_column = "Species", groups_in_facets = T)
+#' ggparci(data = normalize(iris), groups_column = "Species", groups_in_facets = T)
 #'
 #' # flip the plot
-#' p <- ggparci(data = iris, groups_column = "Species", flip_coords = T)
+#' ggparci(data = normalize(iris), groups_column = "Species", flip_coords = T)
 #'
 #' }
 #' @rdname ggparci
 #' @export
 
-ggparci <- function(data, columns = 1:ncol(data), groups_column, conf_level = 0.95, title = "",
-  groups_in_facets = FALSE, flip_coords = FALSE, obs_lines = FALSE, alpha_lines = 0.2,alpha_bands = 0.5,...)
+ggparci <- function(data, columns = 1:ncol(data), groups_column,
+ conf_level = 0.95,
+ title = "",
+ groups_in_facets = FALSE,
+ flip_coords = FALSE,
+ include_obs_lines = FALSE,
+ alpha_obs_lines = 0.1,
+ alpha_ci_bands = 0.5,...)
   {
+
+  # determine which columns are numeric
   numeric_columns <- columns[sapply(data[columns], is.numeric)]
+
+  # cast the groups column to character
   if (is.numeric(data[,groups_column]))
       data[,groups_column] <- as.character(data[,groups_column])
 
   alpha <- 1-conf_level
 
+  # prepare the dataset to plot
   data_to_plot <-
     data %>%
     as_data_frame %>%
     dplyr::select(groups_column, numeric_columns) %>%
-    dplyr::mutate_if(is.numeric,ggparci:::normalize.default) %>%
-    tibble::rownames_to_column("obs_id") %>%
+    # dplyr::mutate_if(is.numeric,ggparci:::normalize.default) %>%
+    tibble::rowid_to_column("obs_id") %>%
     tidyr::gather(
       key = "Variables", value = "value", -!!groups_column, -obs_id)
 
-  if (obs_lines)
+  # prepare the dataset to plot a line for each observation
+  if (include_obs_lines)
     obs_data_to_plot <-
       data_to_plot %>%
       dplyr::group_by(obs_id)
 
+  # also prepare another dataset to plot for each group
   summ_data_to_plot <-
     data_to_plot %>%
     dplyr::group_by(Variables) %>%
@@ -71,19 +85,22 @@ ggparci <- function(data, columns = 1:ncol(data), groups_column, conf_level = 0.
       L = sort(value)[max(qbinom(alpha/2, n(), 0.5), 1)],
       U = sort(value)[qbinom((1-alpha/2), n(), 0.5)])
 
+  # create the plot object and assign the commun aesthetics
   p <- ggplot() +
     aes( x = Variables ) +
     aes_string( color = groups_column )
 
-  if (obs_lines)
+  # add obs lines to plot (this one comes first to include the correct range)
+  if (include_obs_lines)
   {
     p <- p +
       geom_line(
         data = obs_data_to_plot,
         aes(group = obs_id, y = value),
-        show.legend = FALSE, alpha = alpha_lines)
+        show.legend = FALSE, alpha = alpha_obs_lines)
   }
 
+  # plot the rest - median line and ci ribbon
   p <- p +
     aes_string(
     color = groups_column,
@@ -91,16 +108,20 @@ ggparci <- function(data, columns = 1:ncol(data), groups_column, conf_level = 0.
     group = groups_column) +
     geom_line(
       data = summ_data_to_plot,
-      aes(y = median)) +
+      aes(y = median), size = 1.5, ...) +
     geom_ribbon(
       data = summ_data_to_plot,
       aes(ymin = L, ymax = U),
-      alpha = alpha_bands, color =  adjustcolor( "grey", alpha.f = alpha_bands)) +
+      alpha = alpha_ci_bands, color =  adjustcolor( "grey", alpha.f = alpha_ci_bands)) +
     labs(title = title) +
-    scale_y_continuous(breaks = c((0:10)/10)) +
     scale_x_discrete(expand = c(0.05,0))
 
-  p <- p + theme_bw()
+  if (min(data_to_plot$value) >= 0 & max(data_to_plot$value) <= 1 &
+      (max(data_to_plot$value) - min(data_to_plot$value)) > .5 )
+    p <- p + scale_y_continuous(breaks = c((0:10)/10))
+
+
+  # p <- p + theme_bw()
 
   if(groups_in_facets)
     p <- p + facet_grid(reformulate(groups_column,".")) + theme(legend.position="none") + scale_x_discrete(expand = waiver())
@@ -136,3 +157,11 @@ ggparci <- function(data, columns = 1:ncol(data), groups_column, conf_level = 0.
   }
   return(p)
 }
+
+
+
+
+
+
+
+
